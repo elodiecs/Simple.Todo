16c790c2efed2639bef1f6a22a4ccc40604731d6
elodiecs <68194843+elodiecs@users.noreply.github.com>
2020-07-19 15:41:53 +0200

    Add files via upload

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
 import { ChangePasswordComponent } from './users/change-password/change-password.component';
-
+//import { CreateTaskComponent } from './tasks/task.component';
 @NgModule({
     imports: [
         RouterModule.forChild([
@@ -21,7 +21,8 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
                     { path: 'about', component: AboutComponent },
-                    { path: 'update-password', component: ChangePasswordComponent }
+                    { path: 'update-password', component: ChangePasswordComponent },
+                  // { path: 'create-task', component: CreateTaskComponent }
                 ]
             }
         ])
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,18 +1,90 @@
 import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import {
   TaskServiceProxy,
   GetTasksOutput
 } from '@shared/service-proxies/service-proxies';
+import { CreateTaskDialogComponent } from '../tasks/create-task/create-task-dialog.component';
+import { UpdateTaskDialogComponent } from '../tasks/update-task/update-task-dialog.component';
+import { TasksComponent } from '../tasks.component';
 
 @Component({
+  selector: 'home.component',
+  providers: [TaskServiceProxy],
   templateUrl: './home.component.html',
   animations: [appModuleAnimation()],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class HomeComponent extends AppComponentBase {
-  constructor(injector: Injector) {
+GetTasksOutput = new GetTasksOutput();
+  tasks: TasksDto[] ;
+   public count: number;
+   constructor(
+    injector: Injector,
+    private _taskService: TaskServiceProxy,
+    private _modalService: BsModalService,
+   ) {
     super(injector);
   }
+  /*
+
+  //
+  
+   list(
+    request: PagedTasksRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    request.keyword = this.keyword;
+
+    this._taskService
+      .getTasks(request.keyword, request.skipCount, request.maxResultCount)
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: TaskDtoPagedResultDto) => {
+        this.tasks = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  
+ };
+ 
+  public createTask(): void {
+    this.showCreateTaskDialog();
+  }
+
+  
+  showCreateTaskDialog(): void {
+ // let showCreateTaskDialog: BsModalRef;
+  //  createTask = this._modalService.show(
+   //     CreateTaskDialogComponent,
+   //     {
+    //      class: 'modal-lg',
+     //     initialState: {
+            //id: id,
+      //    },
+      //  }
+   //   );
 }
+
+
+    */
+	 public updateTask(task: TaskDto):void{
+	  console.log(task.id);
+	  }
+
+       OnInit(): void {  
+
+ count  = 25; //GetActiveTaskCount();
+
+};
+
+
+      
+      }
+
