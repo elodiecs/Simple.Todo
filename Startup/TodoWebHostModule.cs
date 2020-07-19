150a270b037ae35787f48722eb5ac7cb45d2f896
elodiecs <68194843+elodiecs@users.noreply.github.com>
2020-07-19 16:44:13 +0200

    Add files via upload

---
+++ Startup/TodoWebHostModule.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Simple.Todo.Configuration;
+
+namespace Simple.Todo.Web.Host.Startup
+{
+    [DependsOn(
+       typeof(TodoWebCoreModule))]
+    public class TodoWebHostModule: AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public TodoWebHostModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(TodoWebHostModule).GetAssembly());
+        }
+    }
+}
